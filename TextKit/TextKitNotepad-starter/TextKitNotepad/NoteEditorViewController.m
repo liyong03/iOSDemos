6aec3a03fa935824b25a457eefdf713ae99fdf18
liyong03 <liyong03@gmail.com>
2014-05-04 00:41:44 +0800

    add customized storage

--- TextKit/TextKitNotepad-starter/TextKitNotepad/NoteEditorViewController.m
+++ TextKit/TextKitNotepad-starter/TextKitNotepad/NoteEditorViewController.m
@@ -9,36 +9,73 @@
 #import "NoteEditorViewController.h"
 #import "Note.h"
 #import "TimeIndicatorView.h"
+#import "SyntaxHighlightTextStorage.h"
 
 @interface NoteEditorViewController () <UITextViewDelegate>
-
-@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
 @implementation NoteEditorViewController
 {
     TimeIndicatorView* _timeView;
+    SyntaxHighlightTextStorage* _textStorage;
+    UITextView* _textView;
+    CGSize _keyboardSize;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self createTextView];
     
     _timeView = [[TimeIndicatorView alloc] init:self.note.timestamp];
     [self.view addSubview:_timeView];
+    _textView.scrollEnabled = YES;
     
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(preferredContentSizeChanged:)
                                                  name:UIContentSizeCategoryDidChangeNotification
                                                object:nil];
     
-    self.textView.text = self.note.contents;
-    self.textView.delegate = self;
-    self.textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(keyboardDidShow:)
+     name:UIKeyboardDidShowNotification
+     object:nil];
+    
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(keyboardDidHide:)
+     name:UIKeyboardDidHideNotification
+     object:nil];
+    
+}
+
+- (void)createTextView {
+    NSDictionary* attrs = @{NSFontAttributeName:[UIFont preferredFontForTextStyle:UIFontTextStyleBody]};
+    
+    NSAttributedString* attrString = [[NSAttributedString alloc]
+                                      initWithString:_note.contents
+                                      attributes:attrs];
+    _textStorage = [[SyntaxHighlightTextStorage alloc] init];
+    [_textStorage appendAttributedString:attrString];
+    
+    CGRect newTextViewRect = self.view.bounds;
+    NSLayoutManager* layoutManager = [[NSLayoutManager alloc] init];
+    
+    CGSize containerSize = CGSizeMake(newTextViewRect.size.width, CGFLOAT_MAX);
+    NSTextContainer* container = [[NSTextContainer alloc] initWithSize:containerSize];
+    container.widthTracksTextView = YES;
+    [layoutManager addTextContainer:container];
+    [_textStorage addLayoutManager:layoutManager];
+    
+    _textView = [[UITextView alloc] initWithFrame:newTextViewRect textContainer:container];
+    _textView.delegate = self;
+    [self.view addSubview:_textView];
 }
 
 - (void)viewDidLayoutSubviews {
+    _textView.frame = self.view.bounds;
     [self updateTimeIndicatorFrame];
 }
 
@@ -51,7 +88,8 @@
 }
 
 - (void)preferredContentSizeChanged:(NSNotification*)notification {
-    self.textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    //_textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    [_textStorage update];
     [self updateTimeIndicatorFrame];
 }
 
@@ -59,6 +97,31 @@
 {
     // copy the updated note text to the underlying model.
     _note.contents = textView.text;
+}
+
+- (void)keyboardDidShow:(NSNotification *)nsNotification {
+    NSDictionary *userInfo = [nsNotification userInfo];
+    _keyboardSize = [[userInfo
+                      objectForKey:UIKeyboardFrameBeginUserInfoKey]
+                     CGRectValue].size;
+    [self updateTextViewSize];
+}
+
+- (void)keyboardDidHide:(NSNotification *)nsNotification {
+    _keyboardSize = CGSizeMake(0.0, 0.0);
+    [self updateTextViewSize];
+}
+
+- (void)updateTextViewSize {
+    UIInterfaceOrientation orientation =
+    [UIApplication sharedApplication].statusBarOrientation;
+    CGFloat keyboardHeight =
+    UIInterfaceOrientationIsLandscape(orientation) ?
+    _keyboardSize.width : _keyboardSize.height;
+    
+    _textView.frame = CGRectMake(0, 0,
+                                 self.view.frame.size.width,
+                                 self.view.frame.size.height - keyboardHeight);
 }
 
 @end
---
+++ TextKit/TextKitNotepad-starter/TextKitNotepad/SyntaxHighlightTextStorage.h
@@ -0,0 +1,13 @@
+//
+//  SyntaxHighlightTextStorage.h
+//  TextKitNotepad
+//
+//  Created by Yong Li on 14-5-3.
+//  Copyright (c) 2014年 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyntaxHighlightTextStorage : NSTextStorage
+- (void)update;
+@end
