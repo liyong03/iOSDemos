8ad2025cfb8b9e0ce0c137d5888762273d1bfda9
liyong03 <liyong03@gmail.com>
2014-05-07 22:02:02 +0800

    add AFNetworking and add progress to RAC

--- testReactiveCocoa/testReactiveCocoa/testViewController.m
+++ testReactiveCocoa/testReactiveCocoa/testViewController.m
@@ -10,12 +10,15 @@
 #import <ReactiveCocoa.h>
 #import <RACEXTScope.h>
 #import <UIControl+RACSignalSupport.h>
+#import <AFNetworking.h>
 
 @interface testViewController ()
 
 @property (nonatomic, strong) UIImageView* imageView;
 @property (nonatomic, strong) UIButton* button;
 @property (nonatomic, strong) UITextField* textField;
+@property (nonatomic, strong) UIProgressView* progressView;
+@property (nonatomic, assign) float progress;
 
 @end
 
@@ -50,6 +53,14 @@
     _textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 500, 320, 30)];
     [self.view addSubview:_textField];
     
+    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 550, 320, 10)];
+    [self.view addSubview:_progressView];
+    
+    [[RACObserve(self, progress) deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(NSNumber* number) {
+        NSLog(@"ccc = %@", number);
+        _progressView.progress = number.floatValue;
+    }];
+    
     @weakify(self);
     [[[self downloadImageWithURL:[NSURL URLWithString:@"http://d13yacurqjgara.cloudfront.net/users/306293/screenshots/1475496/___-1.jpg"]]
       deliverOn:[RACScheduler mainThreadScheduler]]
@@ -61,7 +72,7 @@
          NSLog(@"error : %@", error);
      }];
     
-    [self testMerge];
+    //[self testMerge];
 }
 
 - (void)testMerge {
@@ -111,22 +122,52 @@
 
 - (RACSignal*)downloadImageWithURL:(NSURL*)url {
     
-    
-    NSError *error = [NSError errorWithDomain:@"Test"
-                                         code:100
-                                     userInfo:nil];
-    
+//    
+//    NSError *error = [NSError errorWithDomain:@"Test"
+//                                         code:100
+//                                     userInfo:nil];
+    self.progress = 0;
     return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        dispatch_async(dispatch_get_global_queue(0, 0), ^{
-            NSData* data = [NSData dataWithContentsOfURL:url];
-            if(data) {
-                [subscriber sendNext:[UIImage imageWithData:data]];
-                [subscriber sendCompleted];
-            }
-            else {
-                [subscriber sendError:error];
-            }
-        });
+        //dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            NSURLRequest *request = [NSURLRequest requestWithURL:url];
+            AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+            NSProgress *progress;
+            NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request progress:&progress
+                                                                          destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+                                                                              NSLog(@"have response %@", response);
+                                                                              return targetPath;
+                                                                          }
+                                                                    completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+                //[progress removeObserver:self forKeyPath:@"fractionCompleted" context:NULL];
+                if(!error) {
+                    [subscriber sendNext:[UIImage imageWithData:[NSData dataWithContentsOfURL:filePath]]];
+                    [subscriber sendCompleted];
+                }
+                else {
+                    [subscriber sendError:error];
+                }
+            }];
+            
+            [downloadTask resume];
+            [session setDownloadTaskDidWriteDataBlock:^(NSURLSession *session, NSURLSessionDownloadTask *downloadTask, int64_t bytesWritten, int64_t totalBytesWritten, int64_t totalBytesExpectedToWrite) {
+                self.progress = (float)totalBytesWritten/(float)totalBytesExpectedToWrite;
+                NSLog(@"p = %f", self.progress);
+            }];
+            
+//            [progress addObserver:self
+//                       forKeyPath:@"fractionCompleted"
+//                          options:NSKeyValueObservingOptionNew
+//                          context:NULL];
+            
+//            NSData* data = [NSData dataWithContentsOfURL:url];
+//            if(data) {
+//                [subscriber sendNext:[UIImage imageWithData:data]];
+//                [subscriber sendCompleted];
+//            }
+//            else {
+//                [subscriber sendError:error];
+//            }
+        //});
         return nil;
     }];
 }
