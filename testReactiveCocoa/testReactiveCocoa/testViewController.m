4c936eed30b35e599cbdcf8653e7640bfdcf4816
liyong03 <liyong03@gmail.com>
2014-05-12 23:26:38 +0800

    try pause and resume

--- testReactiveCocoa/testReactiveCocoa/testViewController.m
+++ testReactiveCocoa/testReactiveCocoa/testViewController.m
@@ -23,6 +23,8 @@
 @end
 
 @implementation testViewController {
+    NSURLSessionDownloadTask* _downloadTask;
+    NSData* _resumeData;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -39,21 +41,18 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, 320, 240)];
+    _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 80, 320, 240)];
     _imageView.backgroundColor = [UIColor grayColor];
     [self.view addSubview:_imageView];
     
     _button = [UIButton buttonWithType:UIButtonTypeCustom];
-    _button.frame = CGRectMake(0, 450, 320, 40);
+    _button.frame = CGRectMake(0, 320, 320, 40);
     [_button setTitle:@"Click" forState:UIControlStateNormal];
     [_button setTintColor:[UIColor redColor]];
     [_button setBackgroundColor:[UIColor greenColor]];
     [self.view addSubview:_button];
     
-    _textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 500, 320, 30)];
-    [self.view addSubview:_textField];
-    
-    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 550, 320, 10)];
+    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 320, 320, 10)];
     [self.view addSubview:_progressView];
     
     [[RACObserve(self, progress) deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(NSNumber* number) {
@@ -73,6 +72,25 @@
      }];
     
     //[self testMerge];
+    
+    
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 400, 320, 40);
+    [btn setTitle:@"Start" forState:UIControlStateNormal];
+    [btn setTintColor:[UIColor redColor]];
+    [btn setBackgroundColor:[UIColor greenColor]];
+    [btn addTarget:self action:@selector(downloadBigFile:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    
+    UIButton* btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn2.frame = CGRectMake(0, 450, 320, 40);
+    [btn2 setTitle:@"pause" forState:UIControlStateNormal];
+    [btn2 setTintColor:[UIColor redColor]];
+    [btn2 setBackgroundColor:[UIColor greenColor]];
+    [btn2 addTarget:self action:@selector(pause) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn2];
+    
 }
 
 - (void)testMerge {
@@ -110,8 +128,8 @@
           NSLog(@"--- %@", x);
       }]
      subscribeCompleted:^{
-        NSLog(@"============= DONE =============");
-    }];
+         NSLog(@"============= DONE =============");
+     }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -122,65 +140,94 @@
 
 - (RACSignal*)downloadImageWithURL:(NSURL*)url {
     
-//    
-//    NSError *error = [NSError errorWithDomain:@"Test"
-//                                         code:100
-//                                     userInfo:nil];
+    //
+    //    NSError *error = [NSError errorWithDomain:@"Test"
+    //                                         code:100
+    //                                     userInfo:nil];
     self.progress = 0;
     return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
         //dispatch_async(dispatch_get_global_queue(0, 0), ^{
-            NSURLRequest *request = [NSURLRequest requestWithURL:url];
-            AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
-            NSProgress *progress;
-            NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request progress:&progress
-                                                                          destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-                                                                              NSLog(@"have response %@", response);
-                                                                              return targetPath;
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+        NSProgress *progress;
+        NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request progress:&progress
+                                                                      destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+                                                                          [subscriber sendNext:[UIImage imageWithData:[NSData dataWithContentsOfURL:targetPath]]];
+                                                                          return nil;
+                                                                      } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+                                                                          if(!error) {
+                                                                              [subscriber sendCompleted];
                                                                           }
-                                                                    completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
-                //[progress removeObserver:self forKeyPath:@"fractionCompleted" context:NULL];
-                if(!error) {
-                    [subscriber sendNext:[UIImage imageWithData:[NSData dataWithContentsOfURL:filePath]]];
-                    [subscriber sendCompleted];
-                }
-                else {
-                    [subscriber sendError:error];
-                }
-            }];
-            
-            [downloadTask resume];
-            [session setDownloadTaskDidWriteDataBlock:^(NSURLSession *session, NSURLSessionDownloadTask *downloadTask, int64_t bytesWritten, int64_t totalBytesWritten, int64_t totalBytesExpectedToWrite) {
-                self.progress = (float)totalBytesWritten/(float)totalBytesExpectedToWrite;
-                NSLog(@"p = %f", self.progress);
-            }];
-            
-//            [progress addObserver:self
-//                       forKeyPath:@"fractionCompleted"
-//                          options:NSKeyValueObservingOptionNew
-//                          context:NULL];
-            
-//            NSData* data = [NSData dataWithContentsOfURL:url];
-//            if(data) {
-//                [subscriber sendNext:[UIImage imageWithData:data]];
-//                [subscriber sendCompleted];
-//            }
-//            else {
-//                [subscriber sendError:error];
-//            }
+                                                                          else {
+                                                                              [subscriber sendError:error];
+                                                                          }
+                                                                      }];
+        
+        [downloadTask resume];
+        [session setDownloadTaskDidWriteDataBlock:^(NSURLSession *session, NSURLSessionDownloadTask *downloadTask, int64_t bytesWritten, int64_t totalBytesWritten, int64_t totalBytesExpectedToWrite) {
+            self.progress = (float)totalBytesWritten/(float)totalBytesExpectedToWrite;
+            NSLog(@"p = %f", self.progress);
+        }];
+        
+        //            [progress addObserver:self
+        //                       forKeyPath:@"fractionCompleted"
+        //                          options:NSKeyValueObservingOptionNew
+        //                          context:NULL];
+        
+        //            NSData* data = [NSData dataWithContentsOfURL:url];
+        //            if(data) {
+        //                [subscriber sendNext:[UIImage imageWithData:data]];
+        //                [subscriber sendCompleted];
+        //            }
+        //            else {
+        //                [subscriber sendError:error];
+        //            }
         //});
         return nil;
     }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)downloadBigFile:(UIButton*)btn {
+    if(!_downloadTask ) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/MobileHIG/MobileHIG.pdf"]];
+        AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+        NSProgress *progress;
+        _downloadTask = [session downloadTaskWithRequest:request progress:&progress
+                                                                      destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+                                                                          return nil;
+                                                                      } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+                                                                          NSLog(@"done");
+                                                                      }];
+        
+        [_downloadTask resume];
+        [session setDownloadTaskDidWriteDataBlock:^(NSURLSession *session, NSURLSessionDownloadTask *downloadTask, int64_t bytesWritten, int64_t totalBytesWritten, int64_t totalBytesExpectedToWrite) {
+            float progress = (float)totalBytesWritten/(float)totalBytesExpectedToWrite;
+            NSLog(@"p = %f", progress);
+        }];
+    } else {
+        AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+        NSProgress *progress;
+        _downloadTask = [session downloadTaskWithResumeData:_resumeData
+                                                   progress:&progress
+                                                destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+                                                    return nil;
+                                                } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+                                                    NSLog(@"resume done");
+                                                }];
+        [_downloadTask resume];
+        [session setDownloadTaskDidWriteDataBlock:^(NSURLSession *session, NSURLSessionDownloadTask *downloadTask, int64_t bytesWritten, int64_t totalBytesWritten, int64_t totalBytesExpectedToWrite) {
+            float progress = (float)totalBytesWritten/(float)totalBytesExpectedToWrite;
+            NSLog(@"p = %f", progress);
+        }];
+    }
 }
-*/
+
+- (void)pause {
+    if (_downloadTask) {
+        [_downloadTask cancelByProducingResumeData:^(NSData *resumeData) {
+            _resumeData = resumeData;
+        }];
+    }
+}
 
 @end
