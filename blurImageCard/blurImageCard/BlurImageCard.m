1b36e512ef2490115989838e0b0512006401d1b8
liyong03 <liyong03@gmail.com>
2014-07-09 23:01:42 +0800

    add demo showing how to implement iPhone Flickr 3.0 blur image cell

---
+++ blurImageCard/blurImageCard/BlurImageCard.m
@@ -0,0 +1,251 @@
+//
+//  BlurImageCard.m
+//  blurImageCard
+//
+//  Created by Yong Li on 7/9/14.
+//  Copyright (c) 2014 Yong Li. All rights reserved.
+//
+
+#import "BlurImageCard.h"
+
+@interface UIImage (ResizeMagick)
+
+- (UIImage *) resizedImageByMagick: (NSString *) spec;
+- (UIImage *) resizedImageByWidth:  (NSUInteger) width;
+- (UIImage *) resizedImageByHeight: (NSUInteger) height;
+- (UIImage *) resizedImageWithMaximumSize: (CGSize) size;
+- (UIImage *) resizedImageWithMinimumSize: (CGSize) size;
+
+@end
+
+
+@implementation UIImage (ResizeMagick)
+
+// width	Width given, height automagically selected to preserve aspect ratio.
+// xheight	Height given, width automagically selected to preserve aspect ratio.
+// widthxheight	Maximum values of height and width given, aspect ratio preserved.
+// widthxheight^	Minimum values of width and height given, aspect ratio preserved.
+// widthxheight!	Exact dimensions, no aspect ratio preserved.
+// widthxheight#	Crop to this exact dimensions.
+
+- (UIImage *) resizedImageByMagick: (NSString *) spec
+{
+    
+    if([spec hasSuffix:@"!"]) {
+        NSString *specWithoutSuffix = [spec substringToIndex: [spec length] - 1];
+        NSArray *widthAndHeight = [specWithoutSuffix componentsSeparatedByString: @"x"];
+        NSUInteger width = [[widthAndHeight objectAtIndex: 0] longLongValue];
+        NSUInteger height = [[widthAndHeight objectAtIndex: 1] longLongValue];
+        UIImage *newImage = [self resizedImageWithMinimumSize: CGSizeMake (width, height)];
+        return [newImage drawImageInBounds: CGRectMake (0, 0, width, height)];
+    }
+    
+    if([spec hasSuffix:@"#"]) {
+        NSString *specWithoutSuffix = [spec substringToIndex: [spec length] - 1];
+        NSArray *widthAndHeight = [specWithoutSuffix componentsSeparatedByString: @"x"];
+        NSUInteger width = [[widthAndHeight objectAtIndex: 0] longLongValue];
+        NSUInteger height = [[widthAndHeight objectAtIndex: 1] longLongValue];
+        UIImage *newImage = [self resizedImageWithMinimumSize: CGSizeMake (width, height)];
+        return [newImage croppedImageWithRect: CGRectMake ((newImage.size.width - width) / 2, (newImage.size.height - height) / 2, width, height)];
+    }
+    
+    if([spec hasSuffix:@"^"]) {
+        NSString *specWithoutSuffix = [spec substringToIndex: [spec length] - 1];
+        NSArray *widthAndHeight = [specWithoutSuffix componentsSeparatedByString: @"x"];
+        return [self resizedImageWithMinimumSize: CGSizeMake ([[widthAndHeight objectAtIndex: 0] longLongValue],
+                                                              [[widthAndHeight objectAtIndex: 1] longLongValue])];
+    }
+    
+    NSArray *widthAndHeight = [spec componentsSeparatedByString: @"x"];
+    if ([widthAndHeight count] == 1) {
+        return [self resizedImageByWidth: [spec longLongValue]];
+    }
+    if ([[widthAndHeight objectAtIndex: 0] isEqualToString: @""]) {
+        return [self resizedImageByHeight: [[widthAndHeight objectAtIndex: 1] longLongValue]];
+    }
+    return [self resizedImageWithMaximumSize: CGSizeMake ([[widthAndHeight objectAtIndex: 0] longLongValue],
+                                                          [[widthAndHeight objectAtIndex: 1] longLongValue])];
+}
+
+- (CGImageRef) CGImageWithCorrectOrientation
+{
+    if (self.imageOrientation == UIImageOrientationDown) {
+        //retaining because caller expects to own the reference
+        CGImageRetain([self CGImage]);
+        return [self CGImage];
+    }
+    UIGraphicsBeginImageContext(self.size);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    if (self.imageOrientation == UIImageOrientationRight) {
+        CGContextRotateCTM (context, 90 * M_PI/180);
+    } else if (self.imageOrientation == UIImageOrientationLeft) {
+        CGContextRotateCTM (context, -90 * M_PI/180);
+    } else if (self.imageOrientation == UIImageOrientationUp) {
+        CGContextRotateCTM (context, 180 * M_PI/180);
+    }
+    
+    [self drawAtPoint:CGPointMake(0, 0)];
+    
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    UIGraphicsEndImageContext();
+    
+    return cgImage;
+}
+
+
+- (UIImage *) resizedImageByWidth:  (NSUInteger) width
+{
+    CGImageRef imgRef = [self CGImageWithCorrectOrientation];
+    CGFloat original_width  = CGImageGetWidth(imgRef);
+    CGFloat original_height = CGImageGetHeight(imgRef);
+    CGFloat ratio = width/original_width;
+    CGImageRelease(imgRef);
+    return [self drawImageInBounds: CGRectMake(0, 0, width, round(original_height * ratio))];
+}
+
+- (UIImage *) resizedImageByHeight:  (NSUInteger) height
+{
+    CGImageRef imgRef = [self CGImageWithCorrectOrientation];
+    CGFloat original_width  = CGImageGetWidth(imgRef);
+    CGFloat original_height = CGImageGetHeight(imgRef);
+    CGFloat ratio = height/original_height;
+    CGImageRelease(imgRef);
+    return [self drawImageInBounds: CGRectMake(0, 0, round(original_width * ratio), height)];
+}
+
+- (UIImage *) resizedImageWithMinimumSize: (CGSize) size
+{
+    CGImageRef imgRef = [self CGImageWithCorrectOrientation];
+    CGFloat original_width  = CGImageGetWidth(imgRef);
+    CGFloat original_height = CGImageGetHeight(imgRef);
+    CGFloat width_ratio = size.width / original_width;
+    CGFloat height_ratio = size.height / original_height;
+    CGFloat scale_ratio = width_ratio > height_ratio ? width_ratio : height_ratio;
+    CGImageRelease(imgRef);
+    return [self drawImageInBounds: CGRectMake(0, 0, round(original_width * scale_ratio), round(original_height * scale_ratio))];
+}
+
+- (UIImage *) resizedImageWithMaximumSize: (CGSize) size
+{
+    CGImageRef imgRef = [self CGImageWithCorrectOrientation];
+    CGFloat original_width  = CGImageGetWidth(imgRef);
+    CGFloat original_height = CGImageGetHeight(imgRef);
+    CGFloat width_ratio = size.width / original_width;
+    CGFloat height_ratio = size.height / original_height;
+    CGFloat scale_ratio = width_ratio < height_ratio ? width_ratio : height_ratio;
+    CGImageRelease(imgRef);
+    return [self drawImageInBounds: CGRectMake(0, 0, round(original_width * scale_ratio), round(original_height * scale_ratio))];
+}
+
+- (UIImage *) drawImageInBounds: (CGRect) bounds
+{
+    UIGraphicsBeginImageContext(bounds.size);
+    [self drawInRect: bounds];
+    UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resizedImage;
+}
+
+- (UIImage*) croppedImageWithRect: (CGRect) rect {
+    
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGRect drawRect = CGRectMake(-rect.origin.x, -rect.origin.y, self.size.width, self.size.height);
+    CGContextClipToRect(context, CGRectMake(0, 0, rect.size.width, rect.size.height));
+    [self drawInRect:drawRect];
+    UIImage* subImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return subImage;
+}
+
+
+@end
+
+
+@implementation BlurImageCard {
+    UIToolbar* _topBar;
+    UIToolbar* _bottomBar;
+    UIImageView* _contentImageView;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithImage:(UIImage *)image
+{
+    self = [super initWithImage:image];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage
+{
+    self = [super initWithImage:image highlightedImage:highlightedImage];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup {
+    _topBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    _topBar.barStyle = UIBarStyleBlack;
+    _topBar.backgroundColor = [[UIColor colorWithRed:0.3 green:0.3 blue:0.5 alpha:1.0] colorWithAlphaComponent:0.3];
+    _bottomBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    _bottomBar.barStyle = UIBarStyleBlack;
+    _bottomBar.backgroundColor = [[UIColor colorWithRed:0.3 green:0.3 blue:0.5 alpha:1.0] colorWithAlphaComponent:0.3];
+    _contentImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [self addSubview:_contentImageView];
+    [self addSubview:_topBar];
+    [self addSubview:_bottomBar];
+    [self setImage:self.image];
+    _contentImageView.highlightedImage = self.highlightedImage;
+    _contentImageView.clipsToBounds = YES;
+    self.contentMode = UIViewContentModeScaleAspectFill;
+    _contentImageView.contentMode = UIViewContentModeScaleAspectFill;
+    //_contentImageView.hidden = YES;
+    self.clipsToBounds = YES;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    _topBar.frame = CGRectMake(0, 0, self.bounds.size.width, 40);
+    _bottomBar.frame = CGRectMake(0, self.bounds.size.height - 40, self.bounds.size.width, 40);
+    _contentImageView.frame = CGRectMake(0, 40, self.bounds.size.width, self.bounds.size.height - 40 - 40);
+}
+
+- (void)setImage:(UIImage *)image {
+    UIImage* smallImg = [image resizedImageByWidth:4];
+    [super setImage:smallImg];
+    _contentImageView.image = image;
+}
+
+- (void)setHighlightedImage:(UIImage *)highlightedImage {
+    [super setHighlightedImage:highlightedImage];
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+}
+
+@end
