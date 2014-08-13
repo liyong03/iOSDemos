b552ed0d3e7b53b19c335ff8b8653864cbcff997
liyong03 <liyong03@gmail.com>
2014-08-13 15:17:23 +0800

    compose video and image in image picker

--- testImagePicker/testImagePicker/ViewController.h
+++ testImagePicker/testImagePicker/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)openImagePicker:(id)sender;
+
 @end
--- testImagePicker/testImagePicker/ViewController.m
+++ testImagePicker/testImagePicker/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -24,6 +25,27 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)openImagePicker:(id)sender {
+    
+    UIImagePickerController* imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    imagePicker.mediaTypes = @[((__bridge NSString*)kUTTypeImage),((__bridge NSString*)kUTTypeMovie)];
+    imagePicker.allowsEditing = YES;
+    [self presentViewController:imagePicker animated:YES completion:nil];
+    
+}
+
+#pragma mark - ImagePickerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
