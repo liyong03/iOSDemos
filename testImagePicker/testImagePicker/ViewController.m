53b3dc99af0f966ef715e62f8a7235c38e913f93
liyong03 <liyong03@gmail.com>
2014-08-14 22:27:06 +0800

    update image picker demo, still have no good solution

--- testImagePicker/testImagePicker/ViewController.m
+++ testImagePicker/testImagePicker/ViewController.m
@@ -9,19 +9,79 @@
 #import "ViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <AVFoundation/AVFoundation.h>
+#import "CAAnimation+Blocks.h"
 
-@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface YLCameraAnimation : NSObject<UIViewControllerAnimatedTransitioning>
+@property (nonatomic, assign) BOOL isForward;
+@end
+
+@implementation YLCameraAnimation
+
+
+- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext {
+    return 0.25;
+}
+
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+    // 1. Get controllers from transition context
+    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    
+    
+    CGRect startFrame = [transitionContext initialFrameForViewController:fromVC];
+    fromVC.view.frame = startFrame;
+    
+    // 2. Set init frame for toVC
+    //CGRect screenBounds = [[UIScreen mainScreen] bounds];
+    CGRect finalFrame = [transitionContext finalFrameForViewController:toVC];
+    toVC.view.frame = finalFrame;
+    
+    // 3. Add toVC's view to containerView
+    UIView *containerView = [transitionContext containerView];
+    [containerView addSubview:fromVC.view];
+    [containerView addSubview:toVC.view];
+
+    
+    CABasicAnimation* basicScaleAnimation = [CABasicAnimation animationWithKeyPath:@"bounds"];
+    basicScaleAnimation.duration = [self transitionDuration:transitionContext];
+    basicScaleAnimation.beginTime = 0;
+    basicScaleAnimation.fromValue = [NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)];
+    basicScaleAnimation.toValue = [NSValue valueWithCGRect:CGRectMake(0, 0, finalFrame.size.width, finalFrame.size.height)];
+    basicScaleAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    basicScaleAnimation.fillMode = kCAFillModeForwards;
+    basicScaleAnimation.removedOnCompletion = NO;
+    [basicScaleAnimation setCompletion:^(BOOL finished) {
+        
+        [transitionContext completeTransition:YES];
+    }];
+    
+    toVC.view.layer.contentsGravity = kCAGravityResizeAspect;
+    toVC.view.layer.masksToBounds = YES;
+    toVC.view.layer.borderColor = [UIColor redColor].CGColor;
+    toVC.view.layer.borderWidth = 5;
+    [toVC.view.layer addAnimation:basicScaleAnimation forKey:@"Scale"];
+    
+}
+
+@end
+
+
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIViewControllerTransitioningDelegate>
 
 @end
 
 @implementation ViewController {
     AVCaptureSession *captureSession;
+    YLCameraAnimation* _animator;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _animator = [[YLCameraAnimation alloc] init];
     
     [[NSNotificationCenter defaultCenter] addObserverForName:AVCaptureSessionDidStopRunningNotification
                                                       object:nil queue:nil usingBlock:^(NSNotification *note) {
@@ -52,6 +112,16 @@
     }
     
     
+//    imagePicker = [[UIImagePickerController alloc] init];
+//    imagePicker.delegate = self;
+//    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+//    imagePicker.showsCameraControls = NO;
+//    [self.button insertSubview:imagePicker.view belowSubview:self.button.imageView];
+//    imagePicker.view.frame = self.button.bounds;
+//    imagePicker.view.userInteractionEnabled = NO;
+//    [imagePicker viewWillAppear:YES]; // trickery to make it show
+//    [imagePicker viewDidAppear:YES];
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -77,7 +147,33 @@
     imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
     imagePicker.mediaTypes = @[((__bridge NSString*)kUTTypeImage),((__bridge NSString*)kUTTypeMovie)];
     imagePicker.allowsEditing = YES;
-    [self presentViewController:imagePicker animated:YES completion:nil];
+    imagePicker.videoMaximumDuration = 60;
+    //imagePicker.transitioningDelegate = self;
+    
+//    CATransition *transition = [CATransition animation];
+//    transition.duration = 0.5;
+//    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+//    transition.type = kCATransitionReveal;
+//    transition.subtype = kCATransitionFromRight;
+//    [self.view.window.layer addAnimation:transition forKey:nil];
+    
+    [self presentViewController:imagePicker animated:YES completion:^{
+    }];
+    
+//    CGRect frame = [self.view convertRect:imagePicker.view.frame fromView:self.button];
+//    [imagePicker.view removeFromSuperview];
+//    [self.view addSubview:imagePicker.view];
+//    imagePicker.view.frame = frame;
+//    imagePicker.showsCameraControls = YES;
+//    imagePicker.view.userInteractionEnabled = YES;
+//    [UIView animateWithDuration:5.0f
+//                     animations:^{
+//                         imagePicker.view.frame = self.view.bounds;
+//                     } completion:^(BOOL finished) {
+//                         //[imagePicker.view removeFromSuperview];
+//                         //imagePicker.showsCameraControls = YES;
+//                         [self presentViewController:imagePicker animated:NO completion:nil];
+//                     }];
     
 }
 
@@ -88,7 +184,22 @@
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:^{
+        //imagePicker.showsCameraControls = NO;
+//        [self.button insertSubview:imagePicker.view belowSubview:self.button.imageView];
+//        imagePicker.view.frame = self.button.bounds;
+//        imagePicker.view.userInteractionEnabled = NO;
+    }];
 }
+
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source {
+    return _animator;
+}
+
+//- (id <UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed {
+//    return nil;
+//}
+
 
 @end
