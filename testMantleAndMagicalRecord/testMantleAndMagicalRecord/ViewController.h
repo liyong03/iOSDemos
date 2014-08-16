0f77b8c69bc29dde132d148ce65ff0783646e225
liyong03 <liyong03@gmail.com>
2014-08-16 22:59:12 +0800

    init test mantle and magicalrecord

---
+++ testMantleAndMagicalRecord/testMantleAndMagicalRecord/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testMantleAndMagicalRecord
+//
+//  Created by Yong Li on 8/16/14.
+//  Copyright (c) 2014 Yong Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
