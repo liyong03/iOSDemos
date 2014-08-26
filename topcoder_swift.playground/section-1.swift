2da20c3eed80c1bb51811cd0ef1a4717b402f326
liyong03 <liyong03@gmail.com>
2014-08-26 09:59:29 +0800

    finish topcoder swift

--- topcoder_swift.playground/section-1.swift
+++ topcoder_swift.playground/section-1.swift
@@ -19,8 +19,8 @@ R2 – Create a “Customer” struct with a “name” and “email” property
 **/
 // implement code for R2 below
 struct Customer {
-  var name:String
-  var email:String
+    var name:String
+    var email:String
 }
 
 /**
@@ -35,17 +35,17 @@ randomly select a value.)
 // implement code for R3 below
 
 func randomCustomer() -> (name:String, email:String, Int) {
-   let dict:Dictionary<String,String> = [
-    "A":"A@a.com",
-    "B":"B@b.com",
-    "C":"C@c.com",
-    "D":"D@d.com",
-    "E":"E@e.com",
-  ]
-  let names = dict.keys.array
-  let selectedIdx = Int(arc4random_uniform(UInt32(names.count)))
-  let name = names[selectedIdx]
-  return (name, dict[name]!, 3)
+    let dict:Dictionary<String,String> = [
+        "A":"A@a.com",
+        "B":"B@b.com",
+        "C":"C@c.com",
+        "D":"D@d.com",
+        "E":"E@e.com",
+    ]
+    let names = dict.keys.array
+    let selectedIdx = Int(arc4random_uniform(UInt32(names.count)))
+    let name = names[selectedIdx]
+    return (name, dict[name]!, 3)
 }
 
 /**
@@ -71,28 +71,28 @@ return the interpolated string “This product is on sale. It was
 // implement code for R4 below
 
 class Product {
-  let id:Int
-  let name:String
-  let type:String
-  var price:Double
-  var discount:Double
-  var saleStatus:String {
-    get{
-      if self.discount == 0 {
-        return "Sorry. This product is not on sale."
-      } else {
-        return "This product is on sale. It was \(self.price) but with a discount you only pay \(self.price*(1.0-self.discount))."
-      }
+    let id:Int
+    let name:String
+    let type:String
+    var price:Double
+    var discount:Double
+    var saleStatus:String {
+        get{
+            if self.discount == 0 {
+                return "Sorry. This product is not on sale."
+            } else {
+                return "This product is on sale. It was \(self.price) but with a discount you only pay \(self.price*(1.0-self.discount))."
+            }
+        }
     }
-  }
-  
-  init(name:String, price:Double, discount:Double = 0) {
-    self.name = name
-    self.price = price
-    self.discount = discount
-    self.type = types[Int(arc4random_uniform(UInt32(types.count)))]
-    self.id = Int(arc4random_uniform(10001))
-  }
+    
+    init(name:String, price:Double, discount:Double = 0) {
+        self.name = name
+        self.price = price
+        self.discount = discount
+        self.type = types[Int(arc4random_uniform(UInt32(types.count)))]
+        self.id = Int(arc4random_uniform(10001))
+    }
 }
 
 var p = Product(name: "PS4", price: 399)
@@ -117,7 +117,7 @@ var iPhone = Product(name:"chair", price:699, discount: 0.4)
 
 let products = [table, chair, xbox, tv, iPhone]
 for p in products {
-  println(p.saleStatus)
+    println(p.saleStatus)
 }
 
 
@@ -151,53 +151,53 @@ awesome customer!!”
 // implement code for R6 below
 
 class Cart<T> {
-  let customerName:String
-  let customerEmail:String
-  var items:Array<T> = Array<T>()
-  var itemCount:Int {
-    get {
-      return self.items.count
+    let customerName:String
+    let customerEmail:String
+    var items:Array<T> = Array<T>()
+    var itemCount:Int {
+        get {
+            return self.items.count
+        }
     }
-  }
-  var promoCode:String? = nil
-  
-  init(name:String, email:String) {
-    self.customerName = name;
-    self.customerEmail = email;
-  }
-  
-  func add(item:T) {
-    self.items.append(item)
-  }
-  
-  func clear() {
-    self.items.removeAll(keepCapacity: false)
-  }
-  
-  func remove(index:Int) {
-    if index < self.itemCount {
-      self.items.removeAtIndex(index)
+    var promoCode:String? = nil
+    
+    init(name:String, email:String) {
+        self.customerName = name;
+        self.customerEmail = email;
     }
-  }
-  
-  func getPromoCodeDisplay() -> String {
-    if let p = self.promoCode {
-      return "Your promo code is \(p)."
-    } else {
-      return "You do not have a promo code."
+    
+    func add(item:T) {
+        self.items.append(item)
     }
-  }
-  
-  func getCartStatus() -> String {
-    switch self.itemCount {
-    case 0:
-      return "You have no items in your cart."
-    case 1...3:
-      return "You have \(self.itemCount) items in your cart."
-    default:
-      return "You are an awesome customer!!"
+    
+    func clear() {
+        self.items.removeAll(keepCapacity: false)
     }
-  }
+    
+    func remove(index:Int) {
+        if index < self.itemCount {
+            self.items.removeAtIndex(index)
+        }
+    }
+    
+    func getPromoCodeDisplay() -> String {
+        if let p = self.promoCode {
+            return "Your promo code is \(p)."
+        } else {
+            return "You do not have a promo code."
+        }
+    }
+    
+    func getCartStatus() -> String {
+        switch self.itemCount {
+        case 0:
+            return "You have no items in your cart."
+        case 1...3:
+            return "You have \(self.itemCount) items in your cart."
+        default:
+            return "You are an awesome customer!!"
+        }
+    }
 }
 
 
@@ -231,9 +231,17 @@ is 1234.”).
 **/
 // implement code for R8 below
 
-for  {
-  
+for i in 0..<products.count {
+    if i != 3 {
+        let p = products[i]
+        cart.add(p)
+    }
 }
+
+println(cart.getCartStatus())
+println(cart.getPromoCodeDisplay())
+cart.promoCode = "1234"
+println(cart.getPromoCodeDisplay())
 
 
 /**
@@ -242,3 +250,7 @@ be 3) and println the getCartStatus which should display “You have 3 items
 in your cart.”
 **/
 // implement code for R9 below
+
+cart.remove(0)
+println(cart.getCartStatus())
+
