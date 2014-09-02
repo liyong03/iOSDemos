c0e8d80601f12d03c2e85353dff58c3dc768b82d
liyong03 <liyong03@gmail.com>
2014-09-02 22:12:28 +0800

    add test multithreading core data

---
+++ testMultithreadingCoreData/testMultithreadingCoreData/YLViewController.m
@@ -0,0 +1,29 @@
+//
+//  YLViewController.m
+//  testMultithreadingCoreData
+//
+//  Created by Yong Li on 14-9-2.
+//  Copyright (c) 2014年 Yong Li. All rights reserved.
+//
+
+#import "YLViewController.h"
+
+@interface YLViewController ()
+
+@end
+
+@implementation YLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ testMultithreadingCoreData/testMultithreadingCoreDataTests/testMultithreadingCoreDataTests.m
@@ -0,0 +1,34 @@
+//
+//  testMultithreadingCoreDataTests.m
+//  testMultithreadingCoreDataTests
+//
+//  Created by Yong Li on 14-9-2.
+//  Copyright (c) 2014年 Yong Li. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testMultithreadingCoreDataTests : XCTestCase
+
+@end
+
+@implementation testMultithreadingCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
