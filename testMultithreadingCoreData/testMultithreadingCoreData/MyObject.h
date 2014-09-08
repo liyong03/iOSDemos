c37ef75ce4e2c570b69dd62f32f84b4bdbe6e418
liyong03 <liyong03@gmail.com>
2014-09-08 23:09:46 +0800

    test more core data functions

--- testMultithreadingCoreData/testMultithreadingCoreData/MyObject.h
+++ testMultithreadingCoreData/testMultithreadingCoreData/MyObject.h
@@ -2,17 +2,19 @@
 //  MyObject.h
 //  testMultithreadingCoreData
 //
-//  Created by Yong Li on 14-9-2.
+//  Created by Yong Li on 14-9-8.
 //  Copyright (c) 2014年 Yong Li. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class MyObject;
 
 @interface MyObject : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * value;
 @property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * value;
+@property (nonatomic, retain) MyObject *subObjs;
 
 @end
--- testMultithreadingCoreData/testMultithreadingCoreData/MyObject.m
+++ testMultithreadingCoreData/testMultithreadingCoreData/MyObject.m
@@ -2,16 +2,18 @@
 //  MyObject.m
 //  testMultithreadingCoreData
 //
-//  Created by Yong Li on 14-9-2.
+//  Created by Yong Li on 14-9-8.
 //  Copyright (c) 2014年 Yong Li. All rights reserved.
 //
 
+#import "MyObject.h"
 #import "MyObject.h"
 
 
 @implementation MyObject
 
-@dynamic value;
 @dynamic name;
+@dynamic value;
+@dynamic subObjs;
 
 @end
