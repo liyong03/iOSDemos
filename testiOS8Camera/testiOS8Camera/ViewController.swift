fa1819a1e168134818078ca9b01bb4a24552c400
liyong03 <liyong03@gmail.com>
2014-09-25 23:44:51 +0800

    add camera test project

---
+++ testiOS8Camera/testiOS8Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testiOS8Camera
+//
+//  Created by Yong Li on 14-9-25.
+//  Copyright (c) 2014年 Yong Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
