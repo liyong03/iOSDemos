060e6663da0d509866cfc2252d653a982b63f0da
liyong03 <liyong03@gmail.com>
2014-09-26 23:53:42 +0800

    add some test code

--- testiOS8Camera/testiOS8Camera/ViewController.swift
+++ testiOS8Camera/testiOS8Camera/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    private var avsession: AVCaptureSession = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.avsession.sessionPreset = AVCaptureSessionPresetPhoto
+        let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        for d in devices {
+            if let dev = d as? AVCaptureDevice {
+            if dev.hasMediaType(AVMediaTypeVideo) && dev.position == AVCaptureDevicePosition.Back {
+                
+                var error:NSError?
+                let input = AVCaptureDeviceInput.deviceInputWithDevice(dev as AVCaptureDevice, error: &error) as AVCaptureInput
+                self.avsession.addInput(input)
+                
+                let previewLayer = AVCaptureVideoPreviewLayer(session: self.avsession)
+                previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                self.view.layer.addSublayer(previewLayer)
+                
+                self.avsession.startRunning()
+                
+                break
+            }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
